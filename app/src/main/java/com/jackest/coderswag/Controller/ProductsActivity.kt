de54afd8ec6e29c7c7a0d69218a6098413880615
jackest.gong <jackest.gong@bluelab.com>
2019-09-24 09:14:04 +1200

    orientation and size adjusted

--- app/src/main/java/com/jackest/coderswag/Controller/ProductsActivity.kt
+++ app/src/main/java/com/jackest/coderswag/Controller/ProductsActivity.kt
@@ -1,9 +1,11 @@
 package com.jackest.coderswag.Controller
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.GridLayout
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.jackest.coderswag.Adapters.ProductsAdapter
 import com.jackest.coderswag.R
 import com.jackest.coderswag.Services.DataServices
@@ -21,8 +23,21 @@ class ProductsActivity : AppCompatActivity() {
         //println(categoryType)
         adapter = ProductsAdapter(this, DataServices.getProducts(categoryType))
 
+        //check the orientation
+        var spanCount = 2
+        val orientation = resources.configuration.orientation
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE){
+            spanCount = 3
+        }
+
+        //check the screen size
+        val screenSize = resources.configuration.screenWidthDp
+        if (screenSize > 720){
+            spanCount = 3
+        }
+
         //very important to create a layout manager
-        val layoutManager = GridLayoutManager(this, 2)
+        val layoutManager = GridLayoutManager(this, spanCount)//the second one is relative to the orientation
         productsListView.layoutManager = layoutManager
         productsListView.adapter = adapter
     }
